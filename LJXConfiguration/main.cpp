67053f49ef616ef96871a3be88ffb82c0ee48b15
Mahmut Veli <eng.mahmoud.wali@gmail.com>
2026-07-30 16:50:26 +0300

    apply dark theme when installed Qt version is 6.8.x or higher

--- LJXConfiguration/main.cpp
+++ LJXConfiguration/main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 8, 0)
     a.setStyle("Fusion");
     a.styleHints()->setColorScheme(Qt::ColorScheme::Dark);
+#endif
 
     MainWindow w;
     w.show();
